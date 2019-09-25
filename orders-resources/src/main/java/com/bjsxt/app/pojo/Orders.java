d112737e842151c4d3ca26086bcc9bb1e1afb316
rmgj <yan2010zsj@163.com>
2019-09-25 10:31:31 +0800

    初始化项目库

---
+++ orders-resources/src/main/java/com/bjsxt/app/pojo/Orders.java
@@ -0,0 +1,42 @@
+package com.bjsxt.app.pojo;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/***
+ * 订单实体类
+ * @author EDZ
+ */
+public class Orders implements Serializable{
+	
+	private Integer id;
+	private String remark;
+	private Date odate;
+	private Integer total;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	public Date getOdate() {
+		return odate;
+	}
+	public void setOdate(Date odate) {
+		this.odate = odate;
+	}
+	public Integer getTotal() {
+		return total;
+	}
+	public void setTotal(Integer total) {
+		this.total = total;
+	}
+	
+	
+}
